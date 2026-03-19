2ba202814bc171dcd44507d47b61fd54df3d7fbf
cbmytams <sasha@wafia.fr>
2026-03-19 17:06:34 +0100

    Auto-commit: 2026-03-19 17:06:34

--- src/pages/StudioSearchPros.tsx
+++ src/pages/StudioSearchPros.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useMemo, useState } from 'react';
+import { Helmet } from 'react-helmet-async';
 import { useNavigate } from 'react-router-dom';
 import { supabase } from '@/lib/supabase/client';
 
